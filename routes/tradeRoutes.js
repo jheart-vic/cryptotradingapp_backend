56e364c251b335cc7cbc010360cf0ab5c55d04a9
jheart-vic <adebowalevictorjheart@gmail.com>
2025-08-03 15:18:46 +0100

    update tradeRoutes

--- routes/tradeRoutes.js
+++ routes/tradeRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import {auth} from '../middleware/auth.js'
-import { placeTrade } from '../controllers/tradeController.js'
+import { getUserTrades, placeTrade } from '../controllers/tradeController.js'
 
 const router = express.Router()
 
 router.post('/place', auth, placeTrade)
+router.get('/my-trades', auth, getUserTrades)
 
 export default router
